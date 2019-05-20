3f103ba263f2bbe404060cc76e32e6d9243bcecd
hitunes <hitunes247@gmail.com>
2019-05-20 13:09:40 +0100

    React Auth init

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import {render} from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import Layout from './components/common/Layout';
-import HomePage from './components/home/HomePage';
-import AboutPage from './components/about/AboutPage';
-import ContactPage from './components/contact/ContactPage';
-import '../scss/site.scss';
+import Layout from './components/common/Layout'
+import HomePage from './components/home/HomePage'
+import LoginPage from './components/auth/LoginPage'
+import AboutPage from './components/about/AboutPage'
+import ContactPage from './components/contact/ContactPage'
+
+import '../scss/site.scss'
 
 render(
   <Router history={browserHistory}>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={HomePage}/>
-      <Route path="/about" component={AboutPage}/>
-      <Route path="/contact" component={ContactPage}/>
+    <Route path='/' component={Layout}>
+      <IndexRoute component={HomePage} />
+      <Route path='/about' component={AboutPage} />
+      <Route path='/login' component={LoginPage} />
+
+      <Route path='/contact' component={ContactPage} />
     </Route>
   </Router>,
   document.getElementById('app')
